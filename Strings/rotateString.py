376ee2e1e1a3e39069fff27c3d6aac6894533c4e
Inchar R <116972102+Incharar208@users.noreply.github.com>
2023-09-20 21:22:02 +0530

    Create rotateString.py

---
+++ Strings/rotateString.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        if len(s)==len(goal): #initially checking if the length of these strings are equal or not 
+            if goal in s+s: #if equal checking if the goal string present in string+string
+                return True
+
+        else:
+            return False
+
