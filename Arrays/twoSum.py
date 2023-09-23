b2961057cda72e9ef20568c8eaa058df1586cbc0
Inchar R <116972102+Incharar208@users.noreply.github.com>
2023-09-23 13:04:38 +0530

    Update twoSum.py

--- Arrays/twoSum.py
+++ Arrays/twoSum.py
@@ -1,3 +1,10 @@
+# approaches
+# Brute force: for each element compare every othe elements sum
+# Using hashMap
+# it can be solved using two pointers also.
+
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
