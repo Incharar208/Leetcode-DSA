b0c17379f3fbab461b163b3327a7a132a9c3281c
Inchara R <116972102+Incharar208@users.noreply.github.com>
2023-11-25 10:18:22 +0530

    Update rotateString.py

--- Strings/rotateString.py
+++ Strings/rotateString.py
@@ -1,3 +1,4 @@
+# the other names of these questions are: strings rotation of each other ,
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         if len(s)==len(goal): #initially checking if the length of these strings are equal or not 
