6581bc45c89210243fa9994778731930fdfe17c5
Inchara R <116972102+Incharar208@users.noreply.github.com>
2023-12-07 01:58:35 +0530

    Update calculateMoneyInLeetcodeBank.py

--- general/calculateMoneyInLeetcodeBank.py
+++ general/calculateMoneyInLeetcodeBank.py
@@ -1,3 +1,21 @@
+# Time complexity: O(1)
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        if n > 7:
+            count = n // 7 # to find the number of sets with 7 
+            remaining = n % 7 # to find the remaning number of numbers without the 7 set
+            if remaining!=0: # calculating the sum of the elements without 7 set
+                middleSum = (((count+remaining)*(count+remaining+1))/2) - ((count)*(count+1)/2)
+            else:
+                middleSum = 0 # if set of 7 formed then there wont ne any extra numbers
+            totalSum = count * 28 + ((count-1)*(count)/2) * 7 + middleSum # computing the final sum
+        else:
+            totalSum = (n*(n+1)/2) # if value of n lesser than 7 simply calculate the sum uptil that number
+
+        return int(totalSum) # return the totalsum
+
+
+
 # time complexity: O(n)
 class Solution:
     def totalMoney(self, n: int) -> int:
