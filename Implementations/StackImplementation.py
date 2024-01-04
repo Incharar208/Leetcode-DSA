2b6e2bf12939dabc624b2baf5d50b45d3d8613e9
Inchara R <116972102+Incharar208@users.noreply.github.com>
2024-01-05 00:37:30 +0530

    Create StackImplementation.py

---
+++ Implementations/StackImplementation.py
@@ -0,0 +1,27 @@
+# import statement
+from queue import LifoQueue
+
+# initializations
+stack = LifoQueue()
+
+# stack size
+# return the size of the stack
+stack.qsize() 
+
+# to check if the stack is empty or not
+# returns true or false
+stack.empty()
+
+# to check if the stack is full or not
+# returns true or false
+stack.full()
+
+# inserting value to the stack
+stack.put(i)
+
+# popping out the value
+# pops out the 
+stack.get()
+
+# returns the top of the stack
+stack.queue[-1]
