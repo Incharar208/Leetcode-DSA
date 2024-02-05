8a24306fb32ee3e877657ec052f0ccdcf29ad86d
Inchara R <116972102+Incharar208@users.noreply.github.com>
2024-02-05 21:03:47 +0530

    Create SetsImplementation.py

---
+++ Implementations/SetsImplementation.py
@@ -0,0 +1,23 @@
+'''
+* STORES ONLY UNIQUE VALUES 
+* unordered collection of items
+* s = set()
+* index based retrieval is not possible in set
+* accessing elements in set:
+* for i in s:
+      print(i)
+* adding element in to set :
+  s.add(10)
+* OPERATIONS ON SET
+1. UNION:
+  Two sets s and r
+  p = s.union(r)
+2. INTERSECTION:
+  Two sets s and r
+  p = s.intersection(r)
+3. DIFFERENCE:
+  Two sets s and r
+  p = s,difference(r)
+  s-r
+  
+      
