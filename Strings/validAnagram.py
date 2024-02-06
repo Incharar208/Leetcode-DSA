f108958b200b56f05b7269803481d9b73beb53b1
Inchara R <116972102+Incharar208@users.noreply.github.com>
2024-02-06 12:47:01 +0530

    Update validAnagram.py

--- Strings/validAnagram.py
+++ Strings/validAnagram.py
@@ -1,6 +1,30 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # hashing solution
+        # initially we need to check the lengths of both the strings
+        if len(s) != len(t):
+            return False
+        # we initial a dictionary to store the integer values
+        hashMap = defaultdict(int)
+        for i in s:
+            hashMap[i] = hashMap[i] + 1
+
+        for i in t:
+            hashMap[i] = hashMap[i] - 1
+
+        for i in hashMap.values():
+            if i != 0:
+                return False
+        
+        return True
+
+
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if sorted(s) == sorted(t):
             return True
         else:
             return False    
+
